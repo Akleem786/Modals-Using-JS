ecceeb58cc396172912bb005f1c4100e1d71a64f
Akleem786 <97107720+Akleem786@users.noreply.github.com>
2023-02-07 15:21:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener("click", function () {
+    console.log("Button Clicked");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+
+  btnCloseModal.addEventListener("click", closeModal);
+
+  overlay.addEventListener("click", closeModal);
+}
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    if (!modal.classList.contains("hidden")) {
+      closeModal();
+    }
+  }
+});
+ 
